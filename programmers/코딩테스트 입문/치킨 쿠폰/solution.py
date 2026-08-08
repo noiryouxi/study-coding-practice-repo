d1cd7fd991bbf240c5617ad758db3fdbcb6990a5
noiryouxi <noiryouxi@gmail.com>
2026-08-08 15:18:02 +0900

    하루에 한문제

---
+++ programmers/코딩테스트 입문/치킨 쿠폰/solution.py
@@ -0,0 +1,9 @@
+def solution(chicken):
+    answer = 0
+    
+    while chicken >= 10:
+        service = chicken // 10
+        answer += service
+        chicken = service + (chicken % 10)
+    
+    return answer
